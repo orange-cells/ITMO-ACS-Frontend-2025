3b5630194c2ef686fc017bbb5e04c24943b5b316
orange-cells <sokolovaaveraa@gmail.com>
2026-03-02 22:22:59 +0300

    add lab2

---
+++ labs/K3342/Соколова Вера/lab1/api.js
@@ -0,0 +1,15 @@
+const apiLink = "http://localhost:3000";
+
+const api = {
+    getRecipes: () => fetch(`${apiLink}/recipes`).then(res => res.json()),
+    
+    updateUser: async (id, data) => {
+        // Используем обратные кавычки ` для вставки переменной ${id}
+        const res = await fetch(`${apiLink}/users/${id}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+        return await res.json();
+    }
+};
