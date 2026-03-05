3d0749fa7caed80b1c7ddaec61d8a89fc19af12c
orange-cells <sokolovaaveraa@gmail.com>
2026-03-05 20:29:18 +0300

    fix issues & add report

--- labs/K3342/Соколова Вера/lab3/src/composables/useAuth.js
+++ labs/K3342/Соколова Вера/lab3/src/composables/useAuth.js
@@ -2,6 +2,7 @@ import { ref, computed } from 'vue';
 import api from '@/api/instance';
 
 const currentUser = ref(JSON.parse(localStorage.getItem('currentUser')) || null);
+const userId = JSON.parse(localStorage.getItem('currentUser')).id;
 
 export function useAuth() {
   const isLoggedIn = computed(() => !!currentUser.value);
@@ -38,11 +39,12 @@ export function useAuth() {
   const refreshUser = async () => {  // чтобы брать список сохраненных
     if (!currentUser.value) return;
     try {
-      const { data } = await api.get(`/users/${currentUser.value.id}`);
+      const { data } = await api.get(`/users/${userId}`);
       currentUser.value = data;
       localStorage.setItem('currentUser', JSON.stringify(data)); 
     } catch (e) {
-      console.error("ошибка: ", e);
+      console.error("пользователь не найден в db.json");
+      return null;
     }
   };
 
